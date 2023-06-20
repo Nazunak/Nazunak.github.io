74c6db26b12eb984cd68796aaea55c42e65939c0
pavlo <osnova.reiste@gmail.com>
2023-06-20 11:52:52 +0300

    ZxZX

---
+++ script.js
@@ -0,0 +1,168 @@
+// let numberOfFilms;
+// numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотреных фидьмов?', '' );
+// const b = +prompt('На сколько оцените его', '' );
+// const c = prompt('Один из последних просмотреных фидьмов?', '' );
+// const d = +prompt('На сколько оцените его', '' );
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// let temperatyra = -3;
+
+// if (temperatyra > 0) {
+//     console.log("teplo")
+// } else {
+//     console.log("cold")
+// };
+
+// let a = '';
+// let lenght = 7;
+
+// for (let i = 1; i < lenght; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         a += '*';
+//     }
+
+//     a += '\n';
+// }
+
+// console.log(a);
+
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 5; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 20; i >= 10; i--) {
+//     if (i === 13) break;
+//     console.log(i)
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// const arr = [];
+
+// for (i = 5; i <= 10; i++) {
+//     arr[i - 5] = i;    
+// }
+// console.log(arr);
+// return arr;
+
+// let numberOfFilms = +prompt('skolko posmotreli?');
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+
+// for (i = 0; i < 2; i++) {
+//     let a = prompt('odin iz poslednih filmov?');
+//     let b = prompt('ocenite');
+
+//     personalMovieDB.movies[a] = b;
+// }
+
+// console.log(personalMovieDB);
+
+// let numberOfFilms = +prompt('skolko posmotreli filmov?', '');
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt('odin iz poslednih filmov?', '');
+//     let b = +prompt('na skolko ocenite ego?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }   
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log('malo');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('klassic');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('kinoman');
+// } else {
+//     console.log('error');
+// }
+    
+
+
+// let numberF = +prompt('skolko posmorteli?');
+
+// let persMDB = {
+//     count: numberF,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+// let firstLastF = prompt('odin iz poslednih filmov?');
+// let firstOcenka = +prompt('ocenite');
+// let secondLastF = prompt('odin iz poslednih filmov?');
+// let secondOcenka = +prompt('ocenite');
+
+// persMDB.movies[firstLastF] = firstOcenka;
+// persMDB.movies[secondLastF] = secondOcenka;
+
+// console.log(persMDB);
+
+for (let i = 5; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 20; i >= 10; i--) {
+    if (i === 13) break;
+    console.log(i);
+}
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+// let arr = [];
+// for (let i = 5; i <= 10; i++) {
+//     if (arr.)
+// }
